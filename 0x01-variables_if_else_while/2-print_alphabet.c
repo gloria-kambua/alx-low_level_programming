c5fd8db1a7ca8e78a549a2c8506ae0ac87ef933f
kambua <kambuasammy96@gmail.com>
2023-02-20 10:17:12 -0800

    lowercase alphabet

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -1,12 +1,15 @@
 #include <stdlib.h>
 #include <time.h>
+/* more headers goes there */
 
+/* betty style doc for function main goes there */
 int main(void)
 {
 	char ch;
-	for(ch ='a'; ch<='z';ch++)
+
+	for (ch = 'a'; ch <= 'z'; ch++)
 		putchar(ch);
 		putchar('\n');
-	return (0);
 
+	return (0);
 }

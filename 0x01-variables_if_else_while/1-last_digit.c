52ad492ace5041ada1df12f80b99aed6a53fa081
kambua <kambuasammy96@gmail.com>
2023-02-20 09:44:07 -0800

    added if else functionality

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -5,16 +5,18 @@
 /* betty style doc for function main goes there */
 int main(void)
 {
-	int n,x;
+{
+	int n;
 
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
-	x=n%10;
-	if(x>5)
-		printf("Last digit of %d is %d and is greater than 5\n",n,x);
-	else if(x==0)
-		printf("Last digit of %d is %d and is 0\n", n,x);
+
+	if (n % 10 > 5)
+		printf("Last digit of %d is %d and is greater than 5\n", n, n % 10);
+	else if (n % 10 == 0)
+		printf("Last digit of %d is %d and is 0\n", n, n % 10);
 	else
-		printf("Last digit of %d is %d and is less than 6 and not 0\n", n,x);
-	return (0)
+		printf("Last digit of %d is %d and is less than 6 and not 0\n", n, n % 10);
+
+	return (0);
 }
